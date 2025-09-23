d59d51b846dbaed6be8c0f5ab4ac4eb4ef619116
Pritam <pritambbk1982@gmail.com>
2025-09-23 20:56:47 +0530

    File setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chanakya AI Project',
+    version= '0.0.0',
+    author= 'Pritam Ghosh',
+    author_email= 'gpritam.works@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
